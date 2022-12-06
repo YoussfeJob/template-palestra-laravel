9dcfc48e512cf49c1d261d21ff42177717762695
Youssfe Jobrane <juzek.drz@libero.it>
2022-12-07 00:32:32 +0100

    aggiunta hero section
    aggiunta herosection,resa resposive, migliorato
    resposive della navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,10 @@
-<div class="container bg-navbar fixed-top">
-  <nav class="navbar navbar-expand-lg ">
+<div class="container  bg-navbar fixed-top">
+  <nav class="navbar navbar-dark navbar-expand-lg ">
+    <a href="{{route('homepage')}}">
       <img src="/media/yf-logo-no-bg.png" alt="Logo YourFit" height="50">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon" ></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -16,7 +17,7 @@
         <li class="nav-item py-1 px-3 m-1 rounded">
           <a class="text-white nav-link" href="#">Corsi</a>
         </li>
-        <li class="nav-item py-1 px-3 m-1 rounded text-center">
+        <li class="nav-item py-1 px-3 m-1 rounded text-start text-lg-center">
           <a class="text-white nav-link" href="#">Virtual Tour</a>
         </li>
         <li class="nav-item py-1 px-3 m-1 rounded">
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
-    <div class="min-vh-100">
-        <h1 class="text-dark">hello</h1>
+    {{-- hero section --}}
+    <div class="vh-100 overflow-hidden">
+        <div class="hero-bg">
+            <div class="container">
+                <div class="row vh-100 align-items-center">
+                    <div class="col">
+                        <p class="hero-intro">Intro text</p>
+                        <h1 class="hero-text">Titolo della Pagina</h1>
+                        <button class="btn btn-more text-align-center px-4 py-3 text-white">Bottone</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+            <source src="/media/non-completo.mp4" type="video/mp4">
+        </video>
     </div>
 </x-layout>
